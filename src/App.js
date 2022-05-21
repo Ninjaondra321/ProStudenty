30e015d6307660fb794fbb49cac5eb06c31359dd
Ninjaondra321 <ondran321@gmail.com>
2022-05-21 21:27:05 +0200

    Fungujicí zákad, ale nedodelane nested  routes

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Predmet from "./Pages/Predmet";
 import Tema from "./Pages/Tema";
 import Page404 from "./Pages/404";
 import NavBar from "./Components/NavBar";
+import About from "./Pages/About";
 
 import "./Style/ui-kit-z-webu.css";
 import "./Style/style.css";
@@ -26,6 +27,7 @@ function App() {
 
         <Route path="settings" element={<Settings/>} />
         <Route path="404" element={<Page404/>} />
+        <Route path="about" element={<About/>} />
         <Route path=":predmet" element={<Predmet/>} />
         <Route path=":predmet/:tema" element={<Tema/>} />
         
--- src/Pages/Tema.js
+++ src/Pages/Tema.js
@@ -1,14 +1,48 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 
+import { useState } from "react";
+import Markdown from "markdown-to-jsx";
+
+
 const Tema = () => {
-    let { predmet, tema } = useParams();
+  let { predmet, tema } = useParams();
+
+  const [tema_md, setTema_md] = useState('Nacitani...');
+
+
+
+  useEffect(() => {
+    fetch("https://raw.githubusercontent.com/Ninjaondra321/pro-studenty-sources/master/dejepis/napoleon.md")
+      .then(response => response.text())
+      .then(text => setTema_md(text))
+      .then(() => console.log(generateContents()))
+      .catch(error => console.log(error));
+  }
+  )
+
+
+  function generateContents() {
+    let ids = document.querySelectorAll('*[id]');
+    let listOfIds = [];
+    for (let i = 0; i < ids.length; i++) {
+      if (!["root"].includes(ids[i].id)) {
+        listOfIds.push(ids[i].id);
+      }
+    }
+    console.info(listOfIds);
+  }
+
 
   return (
     <div>
       <h1>Predmet</h1>
-        <p>{predmet}</p>
-        <p>{tema} </p>
+      <p>{predmet}</p>
+      <p>{tema} </p>
+
+      <Markdown options={{ slugify: str => str }}>
+        {tema_md}
+      </Markdown>
     </div>
   );
 }
